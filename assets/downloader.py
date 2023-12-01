9b84015927245eac35e42791d38e8f4b445319e3
KalebSchmidlkofer <kaleb@ashbyte.com>
2023-12-01 12:09:16 -0800

    update: started working on web request's

--- assets/downloader.py
+++ assets/downloader.py
@@ -1,6 +1,7 @@
 import yt_dlp
 from typing import Optional
 import os
+from munch import munchify
 class MyLogger:
     def debug(self, msg):
         # For compatibility with youtube-dl, both debug and info are passed into debug
@@ -20,113 +21,62 @@ class MyLogger:
         print(msg)
 
 
-class Downloader():
+class Downloader:
   Started = False
-  def __init__(self, host:Optional[str]=None, host_password:Optional[str]=None, download_path:Optional[str]=None, port:Optional[int]=22):
-    pass
+  web_use = False
+  Status = ''
+  def __init__(self, host:Optional[str]=None, 
+               host_password:Optional[str]=None, 
+               download_path:Optional[str]=None, 
+               port:Optional[int]=22):
+    if not host == None:
+      self.web_use = True
+    else:
+      pass
+
   
-  
-  def progress_hook(d):
+  def progress_hook(self, d):
+    self.Status
     if d['status'] == 'finished':
       print(f'Done Downloading "{d['filename']}"')
+      self.Started = False
     if d['status'] == 'downloading':
-      if Downloader.Started:
+      if self.Started:
         print(d['_percent_str'], d['_eta_str'])
       else: 
-        print(f'Downloading "{d['tmpfilename']}"')
+        print(f'Now Downloading "{d['tmpfilename']}"')
         print(d['_percent_str'], d['_eta_str'])
-        Downloader.Started = True
+        self.Started = True
         
-  def postprocessor_hooks(d):
+  def postprocessor_hooks(self, d):
     if d['status'] == 'started':
+      self.Status = 'Started'
       pass
-      # print('PostProcessing started')
     if d['status'] == 'finished':
+      self.Status = 'Finished'
       pass
-      # print('PostProcessing finished')
 
-  ydl_opts = {
+  def ydl_opts(self):
+    ydl_opts = {
       'logger': MyLogger(),
       'breakonexisting': True,
-      'progress_hooks': [progress_hook],
-      'postprocessor_hooks': [postprocessor_hooks],
+      'progress_hooks': [self.progress_hook],
+      'postprocessor_hooks': [self.postprocessor_hooks],
       'format': 'bestaudio/best',
       'writethumbnail': True,
-      'outtmpl': '%(playlist_title)s/%(title)s.%(ext)s',
+      'outtmpl': 'downloads/%(playlist_title)s/%(title)s.%(ext)s',
       'postprocessors': [
         {'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
-        'preferredquality': 'None'},
-        {'add_metadata': 'True', 'key': 'FFmpegMetadata'},
-        {'already_have_thumbnail': False, 'key': 'EmbedThumbnail'}
-      ]}
+      'preferredcodec': 'mp3',
+      'preferredquality': 'None'},
+      {'add_metadata': 'True', 'key': 'FFmpegMetadata'},
+      {'already_have_thumbnail': False, 'key': 'EmbedThumbnail'}
+    ]}
+    return ydl_opts
 
   
   def download(self, urls):
+    
     for x in urls:
-      with yt_dlp.YoutubeDL(self.ydl_opts) as ydl:
+      with yt_dlp.YoutubeDL(self.ydl_opts()) as ydl:
           ydl.download(x)
-  
-
-
-# {'ProgressTemplate': 'progress',
-#  'breakonexisting': True,
-#  'consoletitle': True,
-#  'format': 'bestaudio/best',
-#  'ignoreerrors': True,
-#  'logger': <ytdl.variables.MyLogger object at 0x7f4a9d34a870>,
-#  'noplaylist': True,
-#  'outtmpl': None,
-#  'postprocessors': [{'key': 'FFmpegExtractAudio',
-#                      'preferredcodec': 'mp3',
-#                      'preferredquality': 'None'},
-#                     {'add_metadata': 'True', 'key': 'FFmpegMetadata'},
-#                     {'already_have_thumbnail': True, 'key': 'EmbedThumbnail'}],
-#  'progress_hooks': [<function youtube.hook at 0x7f4a9cafe520>],
-#  'writethumbnail': True}
-
-
-
-# {'ProgressTemplate': 'progress',
-#  'breakonexisting': True,
-#  'consoletitle': True,
-#  'download_archive': None,
-#  'format': 'bestaudio/best',
-#  'ignoreerrors': True,
-#  'logger': <ytdl.variables.MyLogger object at 0x7f4a9d4a11f0>,
-#  'noplaylist': True,
-#  'outtmpl': './Audio//%(uploader)s/%(title)s.%(ext)s',
-#  'postprocessors': [{'add_metadata': 'True', 'key': 'FFmpegMetadata'},
-#                     {'already_have_thumbnail': True, 'key': 'EmbedThumbnail'}],
-#  'progress_hooks': [<function youtube.hook at 0x7f4a9cafe520>],
-#  'writethumbnail': True}
-
-# {'ProgressTemplate': 'progress',
-#  'breakonexisting': True,
-#  'consoletitle': True,
-#  'format': 'remux/best',
-#  'ignoreerrors': True,
-#  'logger': <ytdl.variables.MyLogger object at 0x7f4a9cfc7410>,
-#  'noplaylist': True,
-#  'outtmpl': None,
-#  'postprocessors': [{'key': 'FFmpegExtractAudio',
-#                      'preferredcodec': 'mp4',
-#                      'preferredquality': 'None'},
-#                     {'add_metadata': 'True', 'key': 'FFmpegMetadata'},
-#                     {'already_have_thumbnail': True, 'key': 'EmbedThumbnail'}],
-#  'progress_hooks': [<function youtube.hook at 0x7f4a9cafe520>],
-#  'writethumbnail': True}
-
-# {'ProgressTemplate': 'progress',
-#  'breakonexisting': True,
-#  'consoletitle': True,
-#  'download_archive': None,
-#  'format': 'remux/best',
-#  'ignoreerrors': True,
-#  'logger': <ytdl.variables.MyLogger object at 0x7f4a9e92b500>,
-#  'noplaylist': True,
-#  'outtmpl': './Videos//%(title)s.%(ext)s',
-#  'postprocessors': [{'add_metadata': 'True', 'key': 'FFmpegMetadata'},
-#                     {'already_have_thumbnail': True, 'key': 'EmbedThumbnail'}],
-#  'progress_hooks': [<function youtube.hook at 0x7f4a9cafe520>],
-#  'writethumbnail': True}
--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 from assets.downloader import Downloader
-urls = ['https://music.youtube.com/playlist?list=OLAK5uy_m2uXUd7beuveI7DE9R3di_EWMy_moTJUg&si=le2Q2_NVvjtbbx-X']
-
+import typer
+import requests
+from typing import Optional
+app = typer.Typer()
 Youtube = Downloader(host='host', host_password='''Host_password''', download_path='~/Downloads')
-Youtube.download(urls=urls)
 
+@app.command()
+def audio(
+  urls:Optional[list[str]] = typer.Option(None, '-l', '--link', help='Url to youtube video/playlist'),
+  server:Optional[bool] = typer.Option(False, '-s', '--server', help='Send a web request to pre-configured url using urls as request')
+):
+  print(server)
+  if not server:
+    Youtube.download(urls=urls)
+  else:
+    response = requests.get('http://127.0.0.1:5000/download/nxUpdlgtUzA')
+    if response.status_code == 200:
+      print(response.json())
+    else:
+      print(f'Failure: {response.status_code}')
+    # print(r.status_code)
+    # Send web request to web server
+    
+    pass
 
+if __name__ == "__main__":
+  app()
